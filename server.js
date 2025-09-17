9b9c2116eb7a84c311336f2f938342d7972ecf73
LOGANATHANTECH <vloganathantech@gmail.com>
2025-09-17 11:14:06 +0530

    created structure

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 
-const bodyparser=require('body-parser');
+// const bodyparser=require('body-parser');
 
-const express = require('express');
-const app= express();
+// const express = require('express');
+// const app= express();
 
-app.use("/login",bodyparser.urlencoded());
+// app.use("/login",bodyparser.urlencoded());
 
 // app.use((req,res,next)=>{
 //     console.log("second middileware");
@@ -17,25 +17,25 @@ app.use("/login",bodyparser.urlencoded());
 //     // res.end();
 // });
 
-app.post("/login",(req,res)=>{
-    const userdata=req.body
-    console.log(userdata.username);
-    console.log(
-        ...Object.keys(userdata),
-        Object.values(userdata)
-    );
+// app.post("/login",(req,res)=>{
+//     const userdata=req.body
+//     console.log(userdata.username);
+//     console.log(
+//         ...Object.keys(userdata),
+//         Object.values(userdata)
+//     );
     
 
-    res.send(`your login successfully ! `)
-});
-app.post("/sigin",(req,res)=>{
-    const userdata=req.body
-    console.log(userdata);
-    res.send(`your sigin successfully ! `)
-});
-app.get("/",(req,res)=>{
-    res.send(`<form action="/login" method="post"><input type="text" name="username" id=""><br><br><input type="text" name="password" id=""><br><br><button type="submit">login</button></form>`);
-});
+//     res.send(`your login successfully ! `)
+// });
+// app.post("/sigin",(req,res)=>{
+//     const userdata=req.body
+//     console.log(userdata);
+//     res.send(`your sigin successfully ! `)
+// });
+// app.get("/",(req,res)=>{
+//     res.send(`<form action="/login" method="post"><input type="text" name="username" id=""><br><br><input type="text" name="password" id=""><br><br><button type="submit">login</button></form>`);
+// });
 
     
-app.listen(3000);
+// app.listen(3000);
