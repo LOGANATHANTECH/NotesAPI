be97cd12d61b6c8429b3d155b096254ca4f7f77e
LOGANATHANTECH <vloganathantech@gmail.com>
2025-09-22 10:45:53 +0530

    custom error handling added for preventing wrong endpoint access

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const Express = require('express');
+const CustomError = require('./utils/CustomError')
+const errorHandlingController= require("./controllers/ErrorHandlingController")
+
+
 const app = Express();
 
-const router = require("./routers/allApis");
+const router = require("./controllers/RouterControllers");
 
 app.use("/note",router);
 
@@ -9,5 +13,10 @@ app.get("/", (req, res) => {
     console.log("running");
     res.send("i am working");
 })
-
+app.use('/{*splat}',(req,res,next)=>{
+    const err = new CustomError("no endpoint found",404);
+    next(err);
+    
+})
+app.use(errorHandlingController);
 app.listen(3000);
